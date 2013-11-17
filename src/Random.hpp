f29c885c14882175a27f77290836a31bcf74bca2
rooooooooob <raftias@gmail.com>
2013-11-17 15:17:03 -0800

    moved random/randomf above choose in Random.hpp so that choose actually works

--- src/Random.hpp
+++ src/Random.hpp
@@ -7,16 +7,16 @@
 namespace je
 {
 
+float randomf(float n);
+
+int random(int n);
+
 template <typename T>
 T choose(const std::initializer_list<T>& il)
 {
 	//  May the Lord forgive me
 	return *(il.begin() + (std::size_t) randomf(il.size()));
 }
-
-float randomf(float n);
-
-int random(int n);
 
 }
 
