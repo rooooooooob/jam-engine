7ec3c18648d3824c53d0255ecbbfc3821e53c05b
rooooooooob <raftias@gmail.com>
2013-11-20 00:10:14 -0800

    fixed division by zero in random number gen

--- src/Random.cpp
+++ src/Random.cpp
@@ -5,12 +5,12 @@ namespace je
 // TODO: make these use <random> and be less crap
 float randomf(float n)
 {
-	return ((rand() % 0xFFFF) / (float) 0xFFFF) * n;
+	return n ? ((rand() % 0xFFFF) / (float) 0xFFFF) * n : 0;
 }
 
 int random(int n)
 {
-	return rand() % n;
+	return n ? rand() % n : 0;
 }
 
 }
