ac15c89974f4cbb218fd1f6ad58b133813bc4652
rooooooooob <raftias@gmail.com>
2014-01-24 20:29:00 -0800

    renamed MinMax -> Math and added more functionality. also fixed the includes in pathfinding stuff

--- src/jam-engine/Utility/Math.hpp
+++ src/jam-engine/Utility/Math.hpp
@@ -1,5 +1,5 @@
-#ifndef JE_MINMAX_HPP
-#define JE_MINMAX_HPP
+#ifndef JE_MATH_HPP
+#define JE_MATH_HPP
 
 namespace je
 {
@@ -14,6 +14,28 @@ template <typename T>
 inline T max(T a, T b)
 {
 	return a > b ? a : b;
+}
+
+template <typename T>
+inline T abs(T n)
+{
+	return n > 0 ? n : -n;
+}
+
+template <typename T>
+void limit(T& n, const T& upper)
+{
+	if (n > upper)
+		n = upper;
+}
+
+template <typename T>
+void limit(T& n, const T& lower, const T& upper)
+{
+	if (n > upper)
+		n = upper;
+	else if (n < lower)
+		n = lower;
 }
 
 }
