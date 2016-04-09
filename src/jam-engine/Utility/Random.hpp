fdc88f19ded2c175ce81e2776911a4432b567a6c
rooooooooob <rooooooooob@users.noreply.github.com>
2016-04-09 13:57:36 -0700

    documentation

--- src/jam-engine/Utility/Random.hpp
+++ src/jam-engine/Utility/Random.hpp
@@ -7,10 +7,22 @@
 namespace je
 {
 
+/**
+ * @param n The maximum 
+ * @return a floating point number in the interval [0, n)
+ */
 float randomf(float n);
 
+/**
+ * @parasm n The maximum
+ * @return an integer in the interval [0, n) ie [0, n - 1]
+ */
 int random(int n);
 
+/**
+ * @param il A list of items
+ * @return an item in the input list randomly chosen
+ */
 template <typename T>
 T choose(const std::initializer_list<T>& il)
 {
